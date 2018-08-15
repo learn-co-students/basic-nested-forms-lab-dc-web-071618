0417dd6c2fc22d6cc03114ee985730636c581b38
julianajlk <julianajlk@gmail.com>
2018-08-15 13:06:30 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,16 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    Recipe.create(accepted_params)
+    redirect_to recipes_path
+  end
+
+  def accepted_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
